f70ec3ec9df0a59b880c5f7ec6790f80f764c34a
Propietario <Propietario@Carlos.domain.name>
2022-05-09 10:40:38 +0200

    Prueba nuevos cambios

--- SalesTool2018/src/ca/demo/salestool/Main.java
+++ SalesTool2018/src/ca/demo/salestool/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		SalesData data = new SalesData();
-		
+
 		displayGreeting();
 		data.display();
 
 	}
-	
-	private static void displayGreeting( ) {
-		System.out.println("Hello Happy Sales People");
+
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People, say hooO!");
 		System.out.println("This app shows sales data");
 		System.out.println("Wooot! It pushed");
 	}
